0ef6c8544a345075bc3bf51806b3ad457cd73e11
adrianovirgilio <49097002+adrianovirgilio@users.noreply.github.com>
2019-09-23 18:57:35 -0300

    Add files via upload

--- processa.php
+++ processa.php
@@ -2,11 +2,10 @@
     require_once("classes/banco.class.php");
     require_once("classes/aluno.class.php");
     $objeto = new Aluno();
-    if($objeto->inserir($_GET) == 'ok'){
+    if($objeto->inserirAluno($_GET) == 'ok'){
         echo "Dados Gravados com sucesso!";
     }else{
         echo "Erro ao tentar gravar os dados!";
     }
-
-
+  
 ?>
